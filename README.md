6bc9595c90b1f0a9c7d0022a55cbe375ba475d03
Huckcity <adam.p.gibbons@gmail.com>
2019-04-19 23:56:11 +0100

    Commenting and README writing, minor changes to leaderboard page responsiveness

--- README.md
+++ README.md
@@ -1,3 +1,67 @@
-# Milestone Project Three
 
-To Be Updated
+# RIDDLE ME THIS
+
+[Website](https://pagination.js.org/)
+
+Riddle Me This is a question and answer game developed as an exercise in the Flask framework for Python.
+
+## UX
+
+The UX is very simple, relying entirely on Bootstraps core elements for design. While I enjoyed the challenge of the project, I couldn't come up with a theme to center my design around, and as such it is very basic, but perfectly functional.
+
+## USER STORIES
+
+- As a user I want to be able to specify a username
+- As a user I want my username to identify me and track my progress
+- As a user I want to be given clearly defined instructions
+- As a user I want to be given clear feedback on my input
+
+
+## FEATURES  
+
+- Players are asked to input a username prior to playing the game. This does not need to be unique
+- The Player is then presented with a series of 10 questions
+- Each question allows the player 3 guesses, and the previous guess is displayed to the user in case they forget the guess
+- Each answer awards the player 10 points, up to a maximum of 100 points
+- If the player answers incorrectly 3 times, the game ends and the user is presented with the scoreboard
+- If the player attempts to manipulate the game incorrectly, the game ends and the user is presented with the scoreboard, where they will have been logged and flagged as a cheater
+ - The user can reset the game at any time
+
+## TESTING
+
+The application was tested thouroughly throughout development. All of the following tests were run numerous times:
+ = Attempt to start game with no user name
+ = Attempt to answer question with no input
+ = Attempt to progress game beyond current question through URL manipulation
+ = Attempt to resume a session that has been reset
+ = Attempt to cause bugs by running multiple instances of the game
+
+### Responsiveness
+
+Given that the site is essentially plain Bootstrap markup, it is fully responsive down to the smallest smartphone screen size, with the small exceoption of a slight overflow on the Leaderboard table. 
+
+
+# TECHNOLOGIES
+
+- Python
+- MySQL
+- Flask
+- HTML
+- CSS
+- Bootstrap
+
+# DEPLOYMENT
+ 
+ COMMANDS USED TO DEPLOY PROJECT TO Heroku:
+ 
+- git init (create empty repository)
+- git add ( to add files)
+- git commit -m'' (to commit changes and add message)
+- heroku login (To log in to heroku platform)
+- heroku ps:scale web=1 (to run app on heroku)
+- git push heroku master ( to push local repository to heroku)
+
+
+# CREDITS
+
+
--- app.py
+++ app.py
@@ -1,136 +1,165 @@
 import os
 from datetime import datetime
-from flask import Flask, redirect, render_template, request, session, url_for, flash
+from flask import Flask, redirect, render_template, request, session, url_for
 
-app = Flask(__name__)
-app.secret_key = "qwer1234"
+APP = Flask(__name__)
+APP.secret_key = "qwer1234"
 
 
 def get_question(number):
-    
-    """ Use current turn number with array of odd numbers to determine which line the next question is on"""
+    """ Use current turn number with array of odd
+     numbers to determine which line the next question is on
+     """
     questions_index = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
     current_question = questions_index[number-1]
-    
+
     riddles = open("static/riddles.txt", "r")
-    
+
     counter = 1
-        
+
     while counter < current_question:
-        riddles.readline();
+        riddles.readline()
         counter += 1
 
-    current_question = {"question" : riddles.readline().rstrip(), "answer" : riddles.readline().rstrip()}
-    
+    current_question = {"question": riddles.readline(
+    ).rstrip(), "answer": riddles.readline().rstrip()}
+
     riddles.close()
-    
+
     return current_question
 
-@app.route('/', methods = ["GET", "POST"])
+
+@APP.route('/', methods=["GET", "POST"])
 def index():
-    
+    """
+    Base route, sets session variables for each user
+    """
     if request.method == "POST":
-        
+
         session["username"] = request.form["username"]
         session["score"] = 0
         session["lives"] = 3
         session["current_question_number"] = 1
-        
+
     if "username" in session:
         return redirect(url_for("questions"))
-    
+
     return render_template('index.html')
-    
-@app.route('/questions', methods = ["GET", "POST"])    
+
+
+@APP.route('/questions', methods=["GET", "POST"])
 def questions():
-    
+    """
+    Questions route, tracks current question and redirects appropriately
+    """
     if "username" not in session:
         return redirect(url_for("index"))
-    
+
     if request.method == "POST":
-        
+
         if session["current_question_number"] < 10:
-            
-            session["current_question_number"] +=1
-            
+
+            session["current_question_number"] += 1
+
         else:
-        
+
             return redirect('gameover')
-            
+
     question_id = session["current_question_number"]
-    
+
     return redirect(url_for('question', question_id=question_id))
 
-@app.route('/question/<int:question_id>', methods = ["GET", "POST"])    
+
+@APP.route('/question/<int:question_id>', methods=["GET", "POST"])
 def question(question_id):
-    
+    """
+    Indivdual question route, fetches current question and 
+    watches for mismatch in id to indicate user is trying to cheat
+    """
     if question_id != session["current_question_number"]:
         return redirect('cheaters')
-    
+
     question = get_question(question_id)
-    
+
     return render_template('questions.html', question=question)
 
-@app.route('/gameover')
+
+@APP.route('/gameover')
 def gameover():
- 
+    """
+    Game over route, reached when user answered all ten questions,
+    or when user got a question wrong and lost,
+    determines score based on number of correct answers
+    """
     username = session["username"]
     correct_answers = session["current_question_number"]
-    
-    ## Answer number doesn't increment on final correct answer, 
-    ## so score adjestment needed on all but last
+
+    # Answer number doesn't increment on final correct answer,
+    # so score adjestment needed on all but last
     if correct_answers < 10:
         correct_answers -= 1
-        
+
     score = correct_answers*10
     date = datetime.today().strftime('%x')
-    
+
     with open('static/scoreboard.txt', 'a') as scoreboard:
-        
+
         scoreboard.write('{}, {}, {}'.format(username, score, date))
         scoreboard.write('\n')
-    
+
     session.clear()
-    
+
     return redirect(url_for('scoreboard'))
-    
-@app.route('/scoreboard')
+
+
+@APP.route('/scoreboard')
 def scoreboard():
-    
+    """
+    Scoreboard route, displays scoreboard to users,
+    doesn't require active session to view
+    """
     scoreboard = open("static/scoreboard.txt", "r")
     scores = []
-    
+
     for score in scoreboard:
 
         score_list = score.split(',')
         scores.append(score_list)
-    
-    return render_template('scoreboard.html', scores=scores)
-    
-@app.route('/reset')
-def reset():
-    
-    session.clear()
-    
-    return redirect(url_for('index'))
-    
 
-@app.route('/cheaters')
+    return render_template('scoreboard.html', scores=scores)
+
+
+@APP.route('/reset')
+def reset():
+    """
+    Reset route, clears all session variables and sends user back to index
+    """
+    session.clear()
+
+    return redirect(url_for('index'))
+
+
+@APP.route('/cheaters')
 def cheaters():
-    
+    """
+    Cheaters route, saves the current username and sets their score to
+    cheater flag, then appends data to the scoreboard file for all the world
+    to see
+    """
     username = session["username"]
     score = "CHEATER"
     date = datetime.today().strftime('%x')
-    
+
     with open('static/scoreboard.txt', 'a') as scoreboard:
-        
+
         scoreboard.write('{}, {}, {}'.format(username, score, date))
         scoreboard.write('\n')
-    
+
     session.clear()
-    
+
     return redirect(url_for('scoreboard'))
-    
-    
+
+
 if __name__ == "__main__":
-    app.run(host=os.getenv('IP', "0.0.0.0"), port=int(os.getenv('PORT', "5000")), debug=False)
+    APP.run(host=os.getenv('IP', "0.0.0.0"), port=int(
+        os.getenv('PORT', "5000")), debug=True)
